e12536253094f36c1567af05439bf773232a3692
krishna-rijal1 <krishnarijal2058@gmail.com>
2024-03-28 14:40:38 +0545

    added output for ebs

--- Modules/Ebs/output.tf
+++ Modules/Ebs/output.tf
@@ -1,9 +1,11 @@
 output "app_name" {
+ description = "beanstalk application name"
  value = aws_elastic_beanstalk_application.delpoy_app.name
 }
 
-# output "env_name" {
-#  value = aws_elastic_beanstalk_environment.NodeEnv.name
-# }
+ output "env_name" {
+ description = "elastic beanstalk environment name"
+ value = aws_elastic_beanstalk_environment.NodeEnv.name
+ }
 
 
